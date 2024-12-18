3dcc5bc8bd283c7e1862c0b5f9ecc1beaf1df43e
Kasra Nadri <kasra.n2018@gmail.com>
2024-12-18 19:57:01 +0330

    added author and tag models and registered their relations

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -4,6 +4,20 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 #----------------------------------------------
+#-------------------- TAG ---------------------
+#----------------------------------------------
+class Tag(models.Model):
+    caption = models.CharField(max_length = 20)
+
+#----------------------------------------------
+#------------------- AUTHOR -------------------
+#----------------------------------------------
+class Author(models.Model):
+    first_name = models.CharField(max_length= 100)
+    last_name = models.CharField(max_length= 100)
+    email_address = models.EmailField()
+
+#----------------------------------------------
 #-------------------- POST --------------------
 #----------------------------------------------
 class Post(models.Models):
@@ -13,4 +27,5 @@ class Post(models.Models):
     created_date = models.DateTimeField(auto_now=True)
     slug = models.SlugField(unique = True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    
+    author = models.ForeignKey(Author, on_delete = models.SET_NULL, related_name = 'posts')
+    tags = models.ManyToManyRel(Tag)
