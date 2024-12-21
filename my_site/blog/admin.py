be264b39c16c0c2968e3625adc6601ab59e37c89
Kasra Nadri <kasra.n2018@gmail.com>
2024-12-21 08:33:52 +0330

    configuring Post in admin.py

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -3,6 +3,11 @@ from .models import Post, Author, Tag
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('author', 'tags', 'created_date')
+    list_display = ('title', 'created_date', 'author')
+    prepopulated_fields = {"slug": ("title",)}
+    
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
