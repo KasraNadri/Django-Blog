babb7d0c09908a15474da6f62d7a23ca9e7e66d0
KasraNadri <kasra.n2018@gmail.com>
2024-12-22 07:29:56 +0330

    fetched data for all posts and single post pages

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,17 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import date
 
 from.models import Post
 
 # Create your views here.
-
-
-all_posts = [
-     
-]
-
-def get_date(all_posts):
-    return all_posts
 
 def starting_page(req):
     latest_posts = Post.objects.all().order_by('-created_date')[:3]
@@ -20,12 +12,16 @@ def starting_page(req):
     })
 
 def posts(req):
+    all_posts = Post.objects.all().order_by('-created_date')
     return render(req, 'blog/all-posts.html', {
         'all_posts': all_posts
     })
 
 def post_detail(req, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    try:
+        identified_post = get_object_or_404(Post, slug = slug)
+    except:
+        return
     return render(req, 'blog/post-detail.html', {
         'post': identified_post
     })
